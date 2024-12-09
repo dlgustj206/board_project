73d3ddd5c1e5c1885be1dd7726509fe30eec1991
dlgustj206 <dlgustj206@naver.com>
2024-12-09 18:25:02 +0900

    feat: WebConfig 클래스 추가
    - 파일 업로드 경로 설정을 위한 WebConfig 클래스 생성
    - 파일 업로드 요청 경로 "/upload/**" 설정
    - 실제 파일 저장 경로 "file:///C:/springboot_img/" 설정
    - 파일 접근 및 저장 경로 설정을 통해 이미지 파일 관리 기능 준비

---
+++ src/main/java/com/board/config/WebConfig.java
@@ -0,0 +1,11 @@
+package com.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private String resourcePath = "/upload/**"; // view에서 접근할 경로
+    private String savePath = "file:///C:/springboot_img/"; // 실제 파일 저장 경로
+}
