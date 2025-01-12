c718fb7f6f42cf5520b4a84d3d4635d1c3ce48c1
dlgustj206 <dlgustj206@naver.com>
2025-01-12 16:42:24 +0900

    feat: CommentDTO 클래스 및 Entity -> DTO 변환 메서드 추가
    - 댓글 데이터를 처리하기 위한 CommentDTO 클래스 생성
    - CommentEntity -> CommentDTO 변환 메서드(toCommentDTO) 구현

--- src/main/java/com/board/dto/CommentDTO.java
+++ src/main/java/com/board/dto/CommentDTO.java
@@ -1,5 +1,6 @@
 package com.board.dto;
 
+import com.board.entity.CommentEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,4 +17,15 @@ public class CommentDTO {
     private String commentContents;
     private Long boardId;
     private LocalDateTime commentCreatedTime;
+
+    public static CommentDTO toCommentDTO(CommentEntity commentEntity, Long boardId) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(commentEntity.getId());
+        commentDTO.setCommentWriter(commentEntity.getCommentWriter());
+        commentDTO.setCommentContents(commentEntity.getCommentContents());
+        commentDTO.setCommentCreatedTime(commentEntity.getCreatedTime());
+//      commentDTO.setBoardId(commentEntity.getBoardEntity().getId()); // Service 메서드에 @Transactional 붙여야 함
+        commentDTO.setBoardId(boardId);
+        return commentDTO;
+    }
 }
