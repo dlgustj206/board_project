db34a123015dcf548ae608fa8756d25967f3f764
dlgustj206 <dlgustj206@naver.com>
2025-01-12 15:13:50 +0900

    feat: 댓글 저장 기능 구현
    - CommentService에 save 메서드 추가

--- src/main/java/com/board/service/CommentService.java
+++ src/main/java/com/board/service/CommentService.java
@@ -1,12 +1,31 @@
 package com.board.service;
 
+import com.board.dto.CommentDTO;
+import com.board.entity.BoardEntity;
+import com.board.entity.CommentEntity;
+import com.board.repository.BoardRepository;
 import com.board.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
+    private final BoardRepository boardRepository;
+
+    public Long save(CommentDTO commentDTO) {
+        // 부모 엔티티(BoardEntity) 조회
+        Optional<BoardEntity> optionalBoardEntity = boardRepository.findById(commentDTO.getBoardId());
+        if (optionalBoardEntity.isPresent()) {
+            BoardEntity boardEntity = optionalBoardEntity.get();
+            CommentEntity commentEntity = CommentEntity.toSaveEntity(commentDTO, boardEntity);
+            return commentRepository.save(commentEntity).getId();
+        } else {
+            return null;
+        }
+    }
 }
