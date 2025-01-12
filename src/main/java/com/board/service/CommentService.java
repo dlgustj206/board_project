89980a4c9083a77ea0e629818f9d8c49fc4f39b6
dlgustj206 <dlgustj206@naver.com>
2025-01-12 13:55:45 +0900

    feat: CommentService 클래스 추가
    - 댓글 관련 비즈니스 로직을 처리하기 위한 서비스 클래스 구현

---
+++ src/main/java/com/board/service/CommentService.java
@@ -0,0 +1,12 @@
+package com.board.service;
+
+import com.board.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+}
